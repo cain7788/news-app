ac0c1ee29f70532d4d64117846f8058deddd3044
zhonglanlan <52788990+cain7788@users.noreply.github.com>
2019-09-22 21:02:01 +0800

    配置input框架组件，用于设置input标签的功能
    在这里使用了双向传递的方法，在子组件中动态获取输入框的value值，通过事件返回给父组件，在父组件当中创建data存储输入框的值，再把传入的value覆盖到数据当中。
    还未完成密码框的数据同步，这个双向传递的方法比较繁琐，以后使用v-model

--- src/components/AuthInput.vue
+++ src/components/AuthInput.vue
@@ -1,1 +1,52 @@
 
+<template>
+  <!-- 这个文件是处理input输入框的组件 -->
+  <div>
+  <input type="text" class="input" placeholder="手机号码" :value="value" @input="handleInput" />
+  <input type="password" class="input" placeholder="密码" :value="value" @inputpwd="handleInput" />
+    </div>
+</template>
+
+<script>
+export default {
+
+  // 动态获取到父组件中传递的数据
+  props: ["placeholder", "value", "name", "rule"],
+
+    // data(){
+    //     return {
+    //         list:[]
+    //     }
+    // },
+//   添加事件
+    methods:{
+        // 每当用户在input中输入的时候触发,通过事件将数据传递给父组件
+        handleInput(event){
+            var obj = {}
+            this.$emit("input",event.target.value)
+            console.log(event.target.value);
+            
+        },
+
+    }
+};
+</script>
+
+<style scoped lang="less">
+ .input {
+    // width:100%;
+    width:354px;
+    // height: 38 / 360 * 100vw;
+    height:30px;
+    padding:10px 20px;
+    box-sizing: border-box;
+    background: #fff;
+    border:none;
+    border-bottom: 1px #666 solid;
+    outline: none;
+    font-size: 18px;
+    margin-top: 15px;
+  }
+
+
+</style>
--- src/page/Login.vue
+++ src/page/Login.vue
@@ -1,51 +1,130 @@
 <template>
-<!-- 登录页 -->
-  <div>
-      <!-- 关闭按钮 -->
-      <div class="close">
-        <span class="iconfont iconicon-test"></span>
-      </div>
-      <!-- logo -->
-      <div class="logo">
-        <span class="iconfont iconnew"></span>
+  <!-- 登录页 -->
+  <div class="container">
+    <!-- 关闭按钮 -->
+    <div class="close">
+      <span class="iconfont iconicon-test"></span>
+    </div>
+    <!-- logo -->
+    <div class="logo">
+      <span class="iconfont iconnew"></span>
+    </div>
+
+    <!-- 用户名密码输入框 -->
+    <div class="main">
+      <!-- <div>
+        <input type="text" />
       </div>
 
-      <!-- 用户名密码输入框 -->
       <div>
-        <input type="text">
-      </div>
+        <input type="password" />
+      </div> -->
 
-      <div>
-        <input type="password">
-      </div>
+      <!-- 模板标签当中传入数据 -->
+      <AuthInput :value="username"  @inputuse="handleUserName"></AuthInput>
+      
+      <button class="loginbtn">登录</button>
 
-      <button>登录</button>
+      <span>
+        没有账号?
+        <i>去注册</i> 
+        </span>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
 
-}
+<script>
+// 导入input组件文件
+import AuthInput from '@/components/AuthInput'
+
+export default {
+  data(){
+    return {
+      form: {
+        username:"",
+        password:""
+      }
+    }
+  },
+
+  // 导入框架组件
+  components:{
+    AuthInput
+  },
+
+
+  methods:{
+    // 获取到username中的value的值进行传输
+    handleUserName(value){
+        // 将子组件传过来的当前输入框中的value值赋值给数据中的username
+        this.form.username = value
+        // console.log(this.value);
+        
+    },
+
+
+
+  }
+
+};
 </script>
 
 <style lang="less">
-    .colse {
-      padding: 20px;
-        span {
-          font-size: 27/360*100vw;
-        }
+// scoped只作用于当前组件
+.container {
+  padding: 20px;
+}
+.colse {
+  span {
+    font-size: 27/360 * 100vw;
+  }
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+
+  span {
+    display: block;
+    font-size: 126/360 * 100vw;
+    color: #d81e06;
+  }
+}
+
+.main {
+  // 将样式放到AuthInput文件当中了.
+  // input {
+  //   width:100%;
+  //   height: 38 / 360 * 100vw;
+  //   padding:25px;
+  //   box-sizing: border-box;
+  //   background: #fff;
+  //   border:none;
+  //   border-bottom: 1px #666 solid;
+  //   outline: none;
+  //   font-size: 20px;
+  // }
+
+  span {
+    font-size: 14px;
+    display: block;
+    margin-top: 20px;
+    text-align: right;
+
+    i {
+      color: rgb(92, 169, 233)
     }
-
-    .logo {
-      display: flex;
-      justify-content: center;
-
-      span {
-        display: block;
-        font-size: 126/360*100vw;
-        color: #d81e06
-      }
-    }
-
+  }
+  .loginbtn {
+    font-size: 36 / 635 *100vw;
+    color: #fff;
+    margin-top: 12px;
+    border: none;
+    width: 100%;
+    height: 60 / 635 * 100vw;
+    background-color: #cc3300;
+    border-radius: 50px;
+  }
+}
 </style>
