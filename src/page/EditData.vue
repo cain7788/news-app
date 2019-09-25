48a78f97e181e1a4e036f98aa5d9caf9cbbb5903
zhonglanlan <52788990+cain7788@users.noreply.github.com>
2019-09-25 19:39:18 +0800

    使用vant插件，添加点击弹出窗口修改数据的功能
    在弹窗中更改的时候，使用v-model将数据同步到本地当中存储缓存，点击确认修改的时候提交请求。将获取用户输入的值发送过去进行修改处理。返回res后，更改本地当中的值，渲染最新更改的值

--- src/page/EditData.vue
+++ src/page/EditData.vue
@@ -4,7 +4,12 @@
     <div class="head">
       <img :src="profile.head_img" alt />
     </div>
-    <CellBar title="昵称" :text="profile.nickname"></CellBar>
+
+    <CellBar title="昵称" :text="profile.nickname" @click="show1 = !show1"></CellBar>
+    <van-dialog title="编辑昵称" v-model="show1" show-cancel-button @confirm="handleNickname">
+      <van-field :value="profile.nickname" placeholder="请输入昵称" v-model="nicknameCache" />
+    </van-dialog>
+
     <CellBar title="密码" type="password" :text="profile.password"></CellBar>
     <CellBar title="性别" :text="profile.gender = 1?'男':'女'"></CellBar>
   </div>
@@ -20,7 +25,16 @@ export default {
   },
   data() {
     return {
-      profile: []
+      profile: [],
+
+      show1: false,
+      show2: false,
+      show3: false,
+
+      //   昵称临时值
+      nicknameCache: "",
+      passwordCache: "",
+      genderCache: ""
     };
   },
   methods: {},
@@ -34,22 +48,62 @@ export default {
       },
       method: "POST"
     }).then(res => {
-      console.log(res.data);
-    const {data} = res.data;
+      //   console.log(res.data);
+      const { data } = res.data;
 
-    this.profile = data
+      this.profile = data;
 
-    if(data){
+      //   输入框默认值
+      this.nicknameCache = this.profile.nickname;
+      this.passwordCache = this.profile.password;
+      this.genderCache = this.profile.gender;
+
+      if (data) {
         // 判断有没有头像
-        if(data.head_img){
-            this.profile.head_img = this.$axios.defaults.baseURL + this.profile.head_img;
-            // console.log(this.profile.head_img);
-        }else {
-            this.profile.head_img = './static/default_img.jpg'
+        if (data.head_img) {
+          this.profile.head_img =
+            this.$axios.defaults.baseURL + this.profile.head_img;
+          // console.log(this.profile.head_img);
+        } else {
+          this.profile.head_img = "./static/default_img.jpg";
         }
-    }
-     
+      }
     });
+  },
+
+  methods: {
+    //   点击确认输入框事件
+    handleNickname() {
+      //   console.log(value);
+      // 点击确认后应该获取到当前输入框中的值，替换数据库中对应的值
+      // console.log(this.nicknameCache);
+    
+
+      this.$axios({
+        url: "/user_update/" + localStorage.getItem("user_id"),
+        method: "POST",
+        // 添加头信息
+        headers: {
+          Authorization: localStorage.getItem("token")
+        },
+        data: {nickname:this.nicknameCache},
+      }).then(res => {
+        console.log(res);
+        const {message} = res.data
+        // 当修改成功的时候
+        if(message === "修改成功"){
+            console.log(123);
+            
+            // 替换当前数据当中的nickname
+            this.profile.nickname = this.nicknameCache
+            // 将提示弹窗
+            this.$toast.success(message)
+        }
+
+
+
+      });
+    }
   }
 };
 </script>
