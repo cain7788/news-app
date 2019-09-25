b789e33260091c12875e27316c3f09de38b5af89
zhonglanlan <52788990+cain7788@users.noreply.github.com>
2019-09-26 01:30:49 +0800

    添加密码编辑栏目（复用封装好的请求函数），添加上传头像的功能
    上传头像的功能主要分成两块，第一个是利用vant实现选择文件上传返回一个路径，通过请求将这个路径保存在服务器当中，服务器返回一个路径渲染在页面上，这个端口是上传端口。第二个是将第一步获取到的新图片路径通过请求将{head_img:data.url}这个数据传到服务器中替换用户头像信息。

--- src/page/EditData.vue
+++ src/page/EditData.vue
@@ -3,14 +3,22 @@
     <HeaderNormal title="编辑资料"></HeaderNormal>
     <div class="head">
       <img :src="profile.head_img" alt />
+      <van-uploader :after-read="afterRead" class="afterRead" />
     </div>
 
+    <!-- 编辑昵称 -->
     <CellBar title="昵称" :text="profile.nickname" @click="show1 = !show1"></CellBar>
     <van-dialog title="编辑昵称" v-model="show1" show-cancel-button @confirm="handleNickname">
       <van-field :value="profile.nickname" placeholder="请输入昵称" v-model="nicknameCache" />
     </van-dialog>
 
-    <CellBar title="密码" type="password" :text="profile.password"></CellBar>
+    <!-- 编辑密码 -->
+    <CellBar title="密码" type="password" :text="profile.password" @click="show2 = !show2"></CellBar>
+    <van-dialog title="编辑昵称" v-model="show2" show-cancel-button @confirm="handlePassword">
+      <van-field :value="profile.password" placeholder="请输入密码" v-model="passwordCache" />
+    </van-dialog>
+
+    <!-- 编辑性别 -->
     <CellBar title="性别" :text="profile.gender = 1?'男':'女'"></CellBar>
   </div>
 </template>
@@ -25,7 +33,7 @@ export default {
   },
   data() {
     return {
-      profile: [],
+      profile: {},
 
       show1: false,
       show2: false,
@@ -37,48 +45,47 @@ export default {
       genderCache: ""
     };
   },
-  methods: {},
 
   mounted() {
     this.$axios({
-      url: "/user_update/" + localStorage.getItem("user_id"),
+      url: "/user/" + localStorage.getItem("user_id"),
       // 添加头信息
       headers: {
         Authorization: localStorage.getItem("token")
-      },
-      method: "POST"
+      }
     }).then(res => {
       //   console.log(res.data);
       const { data } = res.data;
-
-      this.profile = data;
-
-      //   输入框默认值
-      this.nicknameCache = this.profile.nickname;
-      this.passwordCache = this.profile.password;
-      this.genderCache = this.profile.gender;
-
+      //   console.log(res);
       if (data) {
+        //   保存到data
+        this.profile = data;
+
+        //   输入框默认值
+        this.nicknameCache = this.profile.nickname;
+        this.passwordCache = this.profile.password;
+        this.genderCache = this.profile.gender;
+        console.log(this.profile.password);
+
         // 判断有没有头像
         if (data.head_img) {
-          this.profile.head_img =
-            this.$axios.defaults.baseURL + this.profile.head_img;
+          this.profile.head_img = this.$axios.defaults.baseURL + this.profile.head_img;
           // console.log(this.profile.head_img);
         } else {
           this.profile.head_img = "./static/default_img.jpg";
         }
       }
     });
+
   },
 
   methods: {
-
     // 封装修改后台数据的请求方法,调用的时候要在前面添加this调用
-    editProfile(data,callback){
-        // 先判断是否存在data，没有的话就不用继续执行了
-        if(!data) return 
+    editProfile(data, callback) {
+      // 先判断是否存在data，没有的话就不用继续执行了
+      if (!data) return;
 
-        this.$axios({
+      this.$axios({
         url: "/user_update/" + localStorage.getItem("user_id"),
         method: "POST",
         // 添加头信息
@@ -87,32 +94,80 @@ export default {
         },
         data
       }).then(res => {
-        const {message} = res.data
+        console.log(res);
+
+        const { message } = res.data;
         // 当修改成功的时候
-        if(message === "修改成功"){
-            
-            // 替换当前数据
-            if(callback){
-                callback()
-            }
-            // this.profile.nickname = this.nicknameCache
-            // 将提示弹窗
-            this.$toast.success(message)
+        if (message === "修改成功") {
+          // 替换当前数据
+          if (callback) {
+            callback();
+          }
+          // this.profile.nickname = this.nicknameCache
+          // 将提示弹窗
+          this.$toast.success(message);
         }
       });
     },
+
+    // 上传图片——————————————————————————————————————————————————————————————————————————————————————————
+    afterRead(file) {
+      // 此时可以自行将文件上传至服务器
+      //console.log(file);        //选择图片后会返回一个对象，包含了生成的
+
+      // 构造表单数据
+      const formData = new FormData();
+      // 通过表单使用append方法追加数据
+      formData.append("file", file.file);
+
+      this.$axios({
+        url: "/upload",
+        method: "POST",
+        headers: {
+          Authorization: localStorage.getItem("token")
+        },
+        data: formData
+      }).then(res => {
+        // console.log(res);
+
+        const { data } = res.data;
+
+        //替换掉用户资料的头像
+        this.profile.head_img = this.$axios.defaults.baseURL + data.url;
+
+        // 到现在为止，提交了图片给服务器保存并且返回了路径在页面上渲染，接着我们还要替换掉用户数据中的头像路径
+        this.$axios({
+            url:"/user_update/"+ localStorage.getItem("user_id"),
+            method:"POST",
+            headers:{
+                Authorization:localStorage.getItem("token")
+            },
+            // 将当前的图片地址替换掉用户中的头像地址
+            data:{head_img:data.url}
+        }).then(res=>{
+            const {message} = res.data;
+
+            if(message === "修改成功"){
+                this.$toast.success(message)
+            }
+        })
+      });
+    },
+
+    // ____________________________________________________________________________________________
     //   点击确认输入框事件
     handleNickname() {
       //   console.log(value);
       // 点击确认后应该获取到当前输入框中的值，替换数据库中对应的值
       // console.log(this.nicknameCache);
-    
-      this.editProfile({nickname:this.nicknameCache},()=>{
-          this.profile.nickname = this.nicknameCache
-      })
 
+      this.editProfile({ nickname: this.nicknameCache }, () => {
+        this.profile.nickname = this.nicknameCache;
+      });
+    },
 
-
+    handlePassword() {
+      this.editProfile({ password: this.passwordCache });
     }
   }
 };
@@ -123,12 +178,24 @@ export default {
   .head {
     display: flex;
     justify-content: center;
-    width: 100%;
+    // width: 100%;
+    position: relative;
+
+    .afterRead {
+      position: absolute;
+      opacity: 0;
+    }
     img {
       width: 70 / 360 * 100vw;
       height: 70 / 360 * 100vw;
       border-radius: 50%;
     }
+  }
+  // 如果要修改第三方组件库的样式时候，需要在前面加上/deep/， 因为组件库的样式不受scoped的影响
+  /deep/ .van-uploader__upload {
+    margin: 0;
+    width: 70 / 360 * 100vw;
+    height: 70 / 360 * 100vw;
   }
 }
 </style>
