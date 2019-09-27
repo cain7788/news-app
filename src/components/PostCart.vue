e85dd501b54c088b5bbb06d6021e9d67cfd90749
zhonglanlan <52788990+cain7788@users.noreply.github.com>
2019-09-28 01:23:04 +0800

    渲染页面结构
    页面中有三种文章类型，第一：单图类型，包含图片少于3张，显示一张，第二：三张及更多图片，显示三张，第三视频类型，只显示一张图，type类型为2的数据。
    注意，最好是将三个结构代码分为左右布局，方便公共样式书写。
    通过v-if和图片数量为条件控制显示的模块代码。

--- src/components/PostCart.vue
+++ src/components/PostCart.vue
@@ -1,38 +1,66 @@
 <template>
-  <div class="posts">
-    <!-- 第一：正常文章（1~2张图片） -->
-
-
-    <div class="post-content">
-      <span>{{post.title}}</span>
-      <img
-        src="https://hbimg.huabanimg.com/838c45038f94015578740daa35b06d3cb9d92eb829ec3-01bYld_fw658"
-        alt
-      />
+  <div>
+    <!-- -------------------------------------------------------------- -->
+    <!-- 第一：正常文章（1~2张图片）判断post中的cover数组数量 -->
+    <div class="posts" v-if="post.cover.length > 0 && post.cover.length < 3 && post.type === 1">
+      <div class="post-content">
+        <span>{{post.title}}</span>
+        <!-- 单图文章cover中只有一项数据,默认取0就可以了 -->
+        <div class="post-img">
+          <img :src="post.cover[0].url" alt />
+        </div>
+      </div>
+      <div class="post-footer">
+        <span>{{post.user.nickname}}</span>
+        <i>{{post.comment_length}}跟帖</i>
+      </div>
     </div>
 
+    <!-- -------------------------------------------------------------- -->
+    <!-- 第一：多图文章（1~2张图片）判断post中的cover数组数量>=3,但只显示3张 -->
+    <div class="posts" v-if="post.cover.length >= 3">
+      <div class="post-content addcss">
+        <span>{{post.title}}</span>
+        <div class="postimgs">
+          <img v-for="(item,index) in post.cover" :key="index" :src="item.url" v-if="index < 3" />
+        </div>
+        <div class="post-footer">
+          <span>{{post.user.nickname}}</span>
+          <i>{{post.comment_length}}跟帖</i>
+        </div>
+      </div>
+    </div>
 
-    <div class="post-footer">
-      <span>火星时报</span>
-      <i>100跟帖</i>
+    <!-- -------------------------------------------------------------- -->
+    <!-- 第一：视频文章 判断post中的cover数组数量=1且type === 1 -->
+    <div class="posts" v-if="post.cover.length === 1 && post.type === 2">
+      <div class="post-content addcss">
+        <span>{{post.title}}</span>
+        <div class="video">
+          <img :src="post.cover[0].url" />
+          <span class="video-layer">
+            <i class="iconfont iconshipin"></i>
+          </span>
+        </div>
+        <div class="post-footer">
+          <span>{{post.user.nickname}}</span>
+          <i>{{post.comment_length}}跟帖</i>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-    props:['post'],
-    // data(){
-    //     return {
+  props: ["post"],
+  // data(){
+  //     return {
 
-    //     }  
-    // },
+  //     }
+  // },
 
-    mounted(){
-        console.log(this.post);
-        
-    }
+  mounted() {}
 };
 </script>
 
@@ -43,6 +71,7 @@ export default {
   border-bottom: 1px solid #ddd;
 
   .post-content {
+    width: 100%;
     display: flex;
     flex-wrap: nowrap;
     justify-content: space-between;
@@ -53,16 +82,62 @@ export default {
       -webkit-box-orient: vertical;
       -webkit-line-clamp: 2;
       overflow: hidden;
-      line-height: 1.7;
+      line-height: 1.5;
     }
 
-    img {
-      display: block;
-      object-fit: cover;
-      width: 120 / 360 * 100vw;
-      height: 75 / 360 * 100vw;
-      margin-left: 10px;
+    .post-img {
+      img {
+        display: block;
+        object-fit: cover;
+        width: 120 / 360 * 100vw;
+        height: 75 / 360 * 100vw;
+        margin-left: 10px;
+      }
     }
+
+    .postimgs {
+      display: flex;
+      justify-content: space-between;
+      margin: 8px 0;
+      img {
+        display: block;
+        width: 33%;
+        height: 80 / 360 * 100vw;
+        object-fit: cover;
+      }
+    }
+
+    .video {
+      position: relative;
+      margin-bottom: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        display: block;
+        width: 100%;
+        height: 170 / 360 * 100vw;
+      }
+
+      .video-layer {
+        position: absolute;
+        background: rgba(0, 0, 0, 0.5);
+        width: 46 / 360 * 100vw;
+        height: 46 / 360 * 100vw;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-shrink: 0;
+        i {
+          color: #fff;
+          font-size: 30px;
+        }
+      }
+    }
+  }
+  .addcss {
+    flex-direction: column;
   }
 
   .post-footer {
