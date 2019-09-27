0f410def73c8493029b02b034c9d42c0b8959ca3
zhonglanlan <52788990+cain7788@users.noreply.github.com>
2019-09-27 15:37:46 +0800

    创建首页（文章列表页面），添加导航条
    使用vant中的tab栏组件添加导航栏的基础功能。
    插件实现的功能，默认显示的栏目（v-model）,粘性布局（sticky），滑动内容区切换栏目（swipeable）
    我们自己需要实现的功能：
    第一：在根据是否为登陆状态（登陆状态的话是多一个关注栏目）都默认先显示“头条”，用到三元判断token使active取值变化
    第二：监听栏目id的变动情况，当id改变的时候，取到对应的id值赋值到本地data中待用。
    第三：提取请求判断，将请求中的登陆状态提取加以判断是否存在，将这个判断添加在{url:"/category"}的后面。请求的时候以这个对象发送请求。

---
+++ src/components/PostCart.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <!-- 第一：正常文章（1~2张图片） -->
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+
+</style>
--- src/page/index.vue
+++ src/page/index.vue
@@ -1,15 +1,135 @@
 <template>
   <div>
-      
+    <!-- 头部搜索栏 -->
+      <div class="head_nav">
+          <span class="iconfont icon-news logo"></span>
+          <!-- 跳转到搜索页面 -->
+          <router-link to="#">
+          <div class="head_search">
+            <span class="iconfont iconsearch"></span>
+            <i>搜索新闻</i> 
+          </div>
+          </router-link>
+          <span class="iconfont iconwode"></span>
+      </div>
+
+    <!-- 导航栏 -->
+    <van-tabs v-model="active" sticky swipeable>
+      <van-tab v-for="(item,index) in categorys" :title="item.name" :key="index">
+        <!-- <p v-for="index in 100" :key="index">内容 {{ index }} </p>  -->
+        <!-- 渲染文章列表 -->
+        
+        
+        <!-- 调用组件渲染 -->
+        <PostCart>
+
+
+        </PostCart>
+        
+
+
+        
+
+
+
+
+
+      </van-tab>
+</van-tabs>
+
+
   </div>
 </template>
 
 <script>
+import PostCart from '@/components/PostCart'
 export default {
+  components:{
+    PostCart,
+  },
+  data(){
+    return {
+      categorys:[],
+      active:localStorage.getItem("token")?1:0,     //判断，当登陆状态的时候，在头条左边显示关注栏目，头条
+                                                    // 的id为1，所以active为1.反之没有关注栏目则为0.
+      cid:999,
+    }
+  },
+
+  watch:{
+    // 监听当前active的状态，表明当前是哪一块栏目，则应该对应显示该栏目的内容
+    // 监听active
+    active(){
+        // 当变更栏目的时候取到对应栏目数据的id赋值给当前的cid。后面根据cid显示对应的内容
+        this.cid = this.categorys[this.active].id
+        // console.log(this.cid);
+    }
+  },
+
+  mounted(){
+
+    const config = {url:"/category"}
+    // 将验证用户是否登陆的模块提取出来,判断当本地中有token的时候才执行验证,否则不验证
+    // 将headers这个限制放在config对象里面,添加一个属性,因在在请求中无法添加判断,所以提取出来.后面直接请求config就可以了.
+    if(localStorage.getItem("token")){
+      config.headers={
+        Authorization: localStorage.getItem("token")
+      }
+    }
+      
+    this.$axios(config,{
+      // url:"/category",
+      // headers:{
+      //   Authorization: localStorage.getItem("token")
+      // }
+    }).then(res=>{
+      console.log(res.data);
+      const {data} = res.data;
+
+      this.categorys = data
+
+    })
+  }
 
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+
+  .head_nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 0 20px;
+    background-color: #ff4b4b;
+    line-height: 49 / 360 * 100vw;
+    color: #fff;
+
+    .logo {
+      
+      font-size: 44 / 360 * 100vw;
+    }
+
+    .head_search {
+      display: flex;
+      flex: 1;
+      width: 210 / 360 *100vw;
+      height: 30 / 360 * 100vw;
+      justify-content: center;
+      align-items: center;
+      background-color: #f97979;
+      border-radius: 15px;
+      font-size: 15px;
+
+    }
+    >span {
+        font-size: 20px;
+      }
+  }
+
+  // /deep/ .van-tabs__nav{
+  //        background:#f6f6f6;
+  //   }
 
 </style>
