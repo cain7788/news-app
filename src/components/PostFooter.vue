21916dd7f602adbba2a8f6d9e086717dd7c2a1a7
zhonglanlan <52788990+cain7788@users.noreply.github.com>
2019-10-05 16:28:40 +0800

    添加输入框输入状态的样式切换
    当输入框获得焦点的时候，通过v-show切换结构样式，当文本框失去焦点的时候切换回之前的状态。textarea中添加自动获得焦点的属性：autofocus，会在切换后判定获得焦点。

--- src/components/PostFooter.vue
+++ src/components/PostFooter.vue
@@ -1,31 +1,54 @@
 <template>
   <div>
-    <div class="footer" >
-      <input type="text" placeholder=" 点击评论 " />
+    <!-- 正常状态 -->
+    <!-- 输入评论页脚, 这里显示隐藏必须要用v-show，原因是为了获得textare的dom元素,v-if的话就没办法获得dom元素了 -->
+    <div class="footer" v-show="!isFocus">
+      <input type="text" placeholder=" 点击评论 " @focus="handleFocus"/>
       <div class="comment">
         <em>{{post.comment_length}}</em>
         <span class="iconfont iconpinglun-"></span>
       </div>
       <!-- 收藏按钮 -->
-      <i class="iconfont iconshoucang" @click="$emit('handleStar')" :class="{star_active:post.has_star}"></i>
+      <i
+        class="iconfont iconshoucang"
+        @click="$emit('handleStar')"
+        :class="{star_active:post.has_star}"
+      ></i>
       <em class="iconfont iconfenxiang"></em>
+    </div>
+
+    <!-- 输入状态 -->
+    <div class="footer-comment" v-show="isFocus">
+      <!-- autofocus，当切换到文本框的时候自动获得焦点的属性 -->
+      <textarea ref="textarea" rows="3" placeholder="发表您的评论" @blur="handleBlur" :autofocus="isFocus"></textarea>
+      <span>发送</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props:["post"],
+  props: ["post"],
 
-  data(){
+  data() {
     return {
+      // 初始化输入框焦点属性
+      isFocus: false
+    };
+  },
+
+  methods: {
+    // 输入框获得焦点的时候
+    handleFocus(){
+      this.isFocus = true
+    },
+
+    // 文本框失去焦点的时候
+    handleBlur(){
+      this.isFocus = false
 
     }
-  },
-
-  methods:{
-    
-  },
+  }
 };
 </script>
 
@@ -35,7 +58,7 @@ export default {
   justify-content: space-between;
   align-items: center;
   padding: 4px 10px;
-  background-color:#fff;
+  background-color: #fff;
   box-sizing: border-box;
   font-size: 20px;
 
@@ -59,9 +82,9 @@ export default {
       display: block;
       position: absolute;
       font-size: 10px;
-      background: rgba(255, 0, 0, .9);
+      background: rgba(255, 0, 0, 0.9);
       border-radius: 50px;
-      padding:0 3px; 
+      padding: 0 3px;
       top: -5px;
     }
   }
@@ -71,6 +94,37 @@ export default {
 
   .star_active {
     color: rgb(255, 0, 0);
+  }
+}
+
+.footer-comment {
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  background-color: #fff;
+  padding: 5px 10px;
+
+  textarea {
+    width: 240 / 360 * 100vw;
+    background: #eee;
+    padding: 10px;
+    box-sizing: border-box;
+    border: none;
+    border-radius: 8px;
+    resize: none;
+  }
+
+  span {
+    font-size: 12px;
+    background: red;
+    width: 60 / 360 * 100vw;
+    height: 26 / 360 * 100vw;
+    line-height: 26 / 360 * 100vw;
+    color: #fff;
+    text-align: center;
+    border-radius: 50px;
+    margin-right: 10px;
   }
 }
 </style>
